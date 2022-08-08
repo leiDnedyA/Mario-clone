478957ee31f3fab28546b714a6338d4166671677
Ayden <aydendiel@gmail.com>
2022-08-08 11:06:47 -0400

    added feature to second test level

--- public/game.js
+++ public/game.js
@@ -753,12 +753,13 @@ sampleLevelPlatforms.push(new Platform([30, 30], [25, 1]));
 let samplePosEvent = new PositionEvent([40, 25], (playerPosition) => {textManager.createText('Use "A", "D", and Space to move', [...playerPosition])}, [5, 5], true, 1000);
 let samplePosEvent2 = new PositionEvent([32, -3], (playerPosition) => { textManager.createText('Press "W" to enter the door!', [...playerPosition])}, [10, 6], true, 1000);
 let samplePosEvent3 = new PositionEvent([18, 14], (playerPosition) => { textManager.createText('Ty for testing my game <3', [...playerPosition]) }, [10, 6], true, 1000);
+let samplePosEvent4 = new PositionEvent([70, 30], (playerPosition)=>{textManager.createText('Woah good job :O', [...playerPosition])}, [5, 5], true, 1000);
 
 let sampleDoor = new Door([34, 1], [1, 2], 1, [18, 10]);
 let sampleDoor2 = new Door([16, 13], [1, 2], 0, [34, -1]);
 
 levelManager.levels.push(new Level([], sampleLevelPlatforms, [40, 25], [[-10, -10], [2 * tilesVisibleVertically + 20, tilesVisibleVertically + 100]], 'backgroundPiano', [samplePosEvent, samplePosEvent2], [sampleDoor]));
 
-levelManager.levels.push(new Level([], [new Platform([15, 15], [10, 1])], [18, 12], [[-10, -10], [2 * tilesVisibleVertically + 20, tilesVisibleVertically + 100]], 'backgroundPiano', [samplePosEvent3], [sampleDoor2]));
+levelManager.levels.push(new Level([], [new Platform([15, 15], [10, 1]), new Platform([70, 30], [3, 1])], [18, 12], [[-10, -10], [2 * tilesVisibleVertically + 20, tilesVisibleVertically + 100]], 'backgroundPiano', [samplePosEvent3, samplePosEvent4], [sampleDoor2]));
 
 start();
