7b8eff245fb87d3847f81b2e665b1796c256304d
Ayden <aydendiel@gmail.com>
2022-08-12 11:58:52 -0400

    [editor] event loading and basic zooming

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,14 +2,22 @@
 const canvas = document.querySelector('#editorCanvas');
 const ctx = canvas.getContext('2d');
 
-var zoomScale = 30;
+var zoomScale = 50;
+var cameraOffset = [0, 0]
 
 var tileSize = window.innerHeight / zoomScale;
 
 var loadedPlatforms = [];
 var loadedEntities = [];
+var loadedDoors = [];
 
 window.addEventListener('resize', _=>{
+    resizeOrZoom();
+})
+window.addEventListener('wheel', (e)=>{
+    let deltaY = Math.sign(e.deltaY);
+    console.log(deltaY);
+    zoomScale += deltaY;
     resizeOrZoom();
 })
 
@@ -20,9 +28,24 @@ function resizeOrZoom(){
 }
 
 const renderer = {
+
+    worldPosToScreenPos: pos => [pos[0] * tileSize, pos[1] * tileSize],
+
+    worldObjToScreenObj: obj => {
+        return [(obj.position[0] + cameraOffset[0]) * tileSize, (obj.position[1] + cameraOffset[1]) * tileSize, (obj.dimensions[0] + cameraOffset[0]) * tileSize, (obj.dimensions[1] + cameraOffset[1]) * tileSize];
+    },
+
+
     draw: function(){
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = 'white';
+        for(let i in loadedPlatforms){
+            let platform = loadedPlatforms[i];
+            ctx.fillRect(...this.worldObjToScreenObj(platform));
+        }
+
     }
 }
 
@@ -30,8 +53,18 @@ const levelLoader = {
 
     levels: [],
 
-    loadLevel: function(level){
+    currentLevelIndex: 0,
 
+    loadLevel: function(index){
+        if(index < this.levels.length){
+            let currentLevel = this.levels[index];
+            this.currentLevelIndex = index;
+
+            loadedPlatforms = [...currentLevel.platforms];
+            loadedEntities = [...currentLevel.entities];
+            loadedDoors = [...currentLevel.doors];
+
+        }
     },
 
 
@@ -48,7 +81,11 @@ function update(){
 }
 
 
+let sampleLevels = generateLevels(tileSize);
 
+levelLoader.levels.push(sampleLevels[0]);
+levelLoader.levels.push(sampleLevels[1]);
 
+levelLoader.loadLevel(0);
 
 start();
