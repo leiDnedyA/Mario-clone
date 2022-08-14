bdb6f3caf22a90c8caff75d1dad9a3be3dbf60cc
Ayden <aydendiel@gmail.com>
2022-08-14 16:54:11 -0400

    [editor] moved editor.js, added mouseover effect, added menu dom element

--- public/js/editor/editor.js
+++ public/js/editor/editor.js
@@ -34,7 +34,46 @@ var loadedPlatforms = [];
 var loadedEntities = [];
 var loadedDoors = [];
 
+const gameObjects = {
+    platform: loadedPlatforms,
+    entity: loadedEntities,
+    door: loadedDoors
+}
+
+const getGameObject = (type, index)=>{
+    switch(type){
+        case 'platform':
+            return loadedPlatforms[index];
+        case 'entity':
+            return loadedEntities[index];
+        case 'door':
+            return loadedDoors[index];
+        default:
+            return null;
+    }
+}
+
+/**
+ * selectedObjectData and mouseoverObjectData contain info about which game objects
+ * are currently selected/moused over.
+ * 
+ * valid values of 'type' include 'platform', 'entity', and 'door'.
+ * 'index' refers to the index of the gameObject in its respective list e.g loadedPlatforms, loadedEntities, loadedDoors.
+ * 
+ */
+var selectedObjectData = {
+    type: '',
+    index: 0,
+    isSelected: false,
+}
+
+var mouseoverObjectData = {
+    type: '',
+    index: 0
+}
+
 const screenPosToWorldPos = pos=>([(pos[0] / tileSize) - cameraOffset[0], (pos[1] / tileSize) - cameraOffset[1]])
+const pointInObj = (point, obj)=>((point[0]>=obj.position[0]) && point[1]>=obj.position[1] && point[0]<= obj.position[0]+obj.dimensions[0] && point[1]<= obj.position[1]+ obj.dimensions[1]);
 
 window.addEventListener('resize', _=>{
     resizeOrZoom();
@@ -71,9 +110,41 @@ window.addEventListener('mouseup', e=>{
 })
 
 window.addEventListener('mousemove', e=>{
+    
+    //mouse dragging navigation
     if(mouseData.isDown){
         cameraOffset[0] += e.movementX / tileSize;
         cameraOffset[1] += e.movementY / tileSize;
+    }
+
+    //selecting gameObjects
+    let mouseWorldPos = screenPosToWorldPos([e.clientX, e.clientY]);
+    let triggered = false;
+
+    let checkMOObject = (type, index)=>{
+        let obj = getGameObject(type, index);
+        if (pointInObj(mouseWorldPos, obj)) {
+            triggered = true;
+            mouseoverObjectData.type = type;
+            mouseoverObjectData.index = index;
+        }
+    }
+
+    for(let i in loadedPlatforms){
+        checkMOObject('platform', i);
+    }
+    for(let i in loadedEntities){
+        if(triggered) break;
+        checkMOObject('entity', i);
+    }
+    for(let i in loadedDoors){
+        if(triggered) break;
+        checkMOObject('door', i);
+    }
+
+    if(!triggered){
+        mouseoverObjectData.type = '';
+        mouseoverObjectData.index = 0;
     }
 })
 
@@ -101,6 +172,28 @@ const renderer = {
             let platform = loadedPlatforms[i];
             ctx.fillRect(...this.worldObjToScreenObj(platform));
         }
+
+        if(mouseoverObjectData.type !== ''){
+            let target = getGameObject(mouseoverObjectData.type, mouseoverObjectData.index);
+            let rect = this.worldObjToScreenObj(target);
+            let padding = 10;
+            for(let i in rect){
+                if(i<=1){
+                    rect[i]-=padding/2;
+                }else{
+                    rect[i]+=padding;
+                }
+            }
+            ctx.strokeStyle = '#ff0000';
+            ctx.globalAlpha = .5;
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.rect(...rect);
+            ctx.stroke();
+            ctx.globalAlpha = 1;
+            
+        }
+
 
     }
 }
