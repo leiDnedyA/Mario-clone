7ad8a272d6649071c51c2e92dcecdcf969d5956c
Ayden <aydendiel@gmail.com>
2022-09-08 11:28:46 -0400

    started writing dropdown controller

---
+++ public/js/editor/levelDropdown.js
@@ -0,0 +1,48 @@
+
+const levelDropdown = {
+    domElement: document.querySelector('#loadedLevelDropdown'),
+
+    currentIndex: 0,
+
+    optionsList: [],
+    
+    getCurrentIndex: function(){
+        return parseInt(this.currentIndex);
+    },
+
+    setCurrentIndex: function(index){
+        this.currentIndex = parseInt(index);
+        domElement.selectedIndex = this.currentIndex;
+    },
+
+    /**
+     * Sets the list of options in the levelDropdown DOM element
+     * 
+     * @param {[string]} newList labels for each option in the dropdown 
+     */
+    setOptionsList: function(newList){
+        this.optionsList = newList;
+
+        this.domElement.innerHTML = '';
+
+        for(let i in this.optionsList){
+            let optionElement = document.createElement('option');
+            optionElement.innerHTML = this.optionsList[i];
+            this.domElement.appendChild(optionElement);
+        }
+
+    },
+
+    onclickCallback: null,
+    
+    init: function(onclickCallback){
+        this.onclickCallback = onclickCallback;
+
+        this.domElement.addEventListener('click', ()=>{
+            this.currentIndex = this.domElement.selectedIndex;
+            this.onclickCallback();
+        });
+
+    },
+
+}
