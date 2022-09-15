093e2531098429bab50099339300495752159dbe
Ayden <aydendiel@gmail.com>
2022-09-15 12:27:17 -0400

    successfully implemented onload function for popup

--- public/js/editor/destinationPopup.js
+++ public/js/editor/destinationPopup.js
@@ -18,7 +18,9 @@ const destinationPopupController = {
 
         if (this.windowObj === null || this.windowObj.closed) {
             this.windowObj = window.open('destination_popup.html', 'doorLocationSelector', 'popup,width=500,height=500');
-            
+            this.windowObj.onload = _=>{
+                loadContents();
+            }
             
         } else {
             this.windowObj.focus();
